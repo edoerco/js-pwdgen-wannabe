a379983a4923b4c7aa7d6014bc6dc7a43af6e255
Edoardo <ercolino.edoardo@gmail.com>
2021-07-14 16:00:19 +0200

    Conclusione Bonus

---
+++ Bonus/main.js
@@ -0,0 +1,12 @@
+var nome = prompt('Inserisci un nome');
+
+document.getElementById('nome').innerHTML = nome 
+
+var animale = prompt('Inserisci un animale');
+
+document.getElementById('animale').innerHTML = animale 
+
+var luogo = prompt('Inserisci ua città');
+
+document.getElementById('luogo').innerHTML = 'a ' + luogo 
+
